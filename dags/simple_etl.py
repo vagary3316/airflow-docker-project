cd0deadce2537bdcfa41147abb75d2eb408c793e
Selina <selina.chen@mdfinstruments.com>
2025-07-09 12:54:09 +0800

    mlb data retrieval

--- dags/simple_etl.py
+++ dags/simple_etl.py
@@ -1,32 +1,54 @@
-
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+from airflow.models import Variables
 from datetime import datetime
 import requests
-import csv
 import boto3
+import pandas as pd
+from io import StringIO
+
 
 def fetch_data():
-    url = "https://jsonplaceholder.typicode.com/posts"
-    response = requests.get(url)
-    data = response.json()
+    url = "https://api.sportsdata.io/v3/mlb/scores/json/AllTeams"
+    api_key = Variables.get('mlb_api_key')
 
-    with open("/opt/airflow/dags/fake_data.csv", "w", newline="") as f:
-        writer = csv.DictWriter(f, fieldnames=["userId", "id", "title", "body"])
-        writer.writeheader()
-        writer.writerows(data)
+    headers = {
+        "Ocp-Apim-Subscription-Key": api_key
+    }
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        teams = response.json()
+        teams_df = pd.json_normalize(teams)
+
+        upload_to_s3(teams_df, bucket="selina-airflow", key="teams_data.csv")
+    else:
+        print("Error", response.status_code)
+
+
+def upload_to_s3(df, bucket,  key):
+    """
+    Upload a df as csv to S3
+    :param df: The df wanna upload
+    :param bucket: the bucket in s3
+    :param key: filename that uploaded to s3
+    :return: None
+    """
     s3 = boto3.client("s3")
-    s3.put_object(Bucket="selina-airflow", Key="test.txt", Body="Hello from Airflow")
+    csv_buffer = StringIO()
+    df.to_csv(csv_buffer, index=False)
+
+    s3.put_object(Bucket=bucket, Key=key, Body=csv_buffer.getvalue())
 
 with DAG(
     dag_id="simple_etl",
-    start_date=datetime(2025, 1, 1),
+    start_date=datetime(2025, 7, 1),
     schedule_interval=None,
     catchup=False,
     tags=["etl"],
 ) as dag:
     t1 = PythonOperator(
-        task_id="fetch_fake_data",
+        task_id="fetch_mlb_data",
         python_callable=fetch_data,
     )
 
