2cd57d7bee5aa2334db3e53e02e96a68e695a0db
Selina <selina.chen@mdfinstruments.com>
2025-07-30 17:11:23 +0800

    dag_catch_prob_data and added rds connection

--- dags/catch_games_by_date.py
+++ dags/catch_games_by_date.py
@@ -174,7 +174,7 @@ def catch_gprob_from_id():
     query = """
     SELECT game_id, datetime_utc, officialDate, teams_home_team_id, teams_away_team_id, status
     FROM mlb_schedule
-    WHERE DATE(officialDate) >= DATE('now', '-2 days')
+    WHERE DATE(officialDate) >= DATE_SUB(CURDATE(), INTERVAL 2 DAY)
     AND status = 'Final';
     """
     # Use read_sql to get a DataFrame directly
