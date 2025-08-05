b05c303bc92f171b62b24deb8410de5657662b26
Selina <selina.chen@mdfinstruments.com>
2025-08-05 14:44:47 +0800

    streamlit - theme

--- 1_mlb_visualization.py
+++ 1_mlb_visualization.py
@@ -1,4 +1,16 @@
 import streamlit as st
 
+## streamlit_setting
+st.set_page_config(layout="wide",
+                   page_title="Data Visualization for MLB data",
+                   page_icon=":baseball:")
 # Streamlit Header and Subheader
 st.header('Data Visualization for MLB data')
+
+# Initialize connection.
+conn = st.connection('mysql', type='sql')
+
+# Perform query.
+df = conn.query('SELECT * from league;', ttl=600)
+
+st.dataframe(df)
