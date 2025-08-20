babf38e0e8fd5a7bd4aa92c00441509c3d02cb31
Selina <selina.chen@mdfinstruments.com>
2025-08-21 00:13:41 +0800

    streamlit - dataframe for schedule

--- 1_mlb_visualization.py
+++ 1_mlb_visualization.py
@@ -7,22 +7,26 @@ st.set_page_config(layout="wide",
 # Streamlit Header and Subheader
 st.header('Data Visualization for MLB data')
 
-# Initialize connection.
+# Initialize connection
 conn = st.connection("my_mysql", type="sql")
 
-# Perform query.
-test_df = conn.query('''
-SELECT officialDate,
-       datetime_utc,
-       seriesGameNumber,
-       home_team,
-       home_score,
-       away_team,
-       away_score
-from mlb_schedule
-where officialDate = (SELECT MAX(officialDate) from mlb_schedule WHERE status = 'Final')
-and status = 'Final';
-'''
-                     , ttl=600)
+# Streamlit Header and Subheader
+st.subheader('Schedule and Scores')
 
-st.dataframe(test_df)
+# Table of Schedule
+schedule_df = conn.query('''
+    SELECT officialDate, datetime_utc, seriesGameNumber, home_team,
+            home_score, away_team,away_score,teams_home_leagueRecord_wins,
+            teams_home_leagueRecord_losses, teams_away_leagueRecord_wins,
+            teams_away_leagueRecord_losses
+    from mlb_schedule
+    where officialDate = (SELECT MAX(officialDate) from mlb_schedule WHERE status = 'Final')
+    and status = 'Final';
+'''
+                         , ttl=600)
+
+schedule_df['teams'] = schedule_df['away_team'] + " @ " + schedule_df['home_team']
+schedule_df['scores'] = schedule_df['away_score'] + "-" + schedule_df['home_score']
+schedule_df = schedule_df[['officialDate', 'datetime_utc', 'teams', 'scores']]
+
+st.dataframe(schedule_df)
